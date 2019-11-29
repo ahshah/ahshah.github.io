a65ed151f7c2c86b954a3e2f765e3cd1891caba8
Ali Shah <ali@titus.local>
2019-11-29 17:32:56 -0500

    Add Meme Generator

---
+++ memes/js/images.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*  
+1. images model.
+2. render images.
+*/
+
+var gNextId = 1;
+var gImgs;
+
+function init() {
+    gImgs = createImgs();
+    renderImgs(gImgs);
+}
+
+function createImgs() {
+    var imgs = [];
+
+   imgs.push(createImage('./img/gallery/One-Does-Not-Simply.jpg', ['fun']))
+
+    return imgs;
+}
+
+function createImage(url, keywords) {
+    return {
+        id: gNextId++,
+        url: url,
+        keywords: keywords
+    };
+}
+
+function renderImgs(imgs) {
+    var strHtml = imgs.map(function (img, idx) {
+        return `
+        <img id='${img.id}' src='${img.url}' onclick="initMemeEditor(${img.id},this)" alt='meme picture'/>
+        `
+    })
+        .join(' ')
+        
+    document.querySelector('.gallery').innerHTML = strHtml;
+}
+
+// <div id='${img.id}' class="photo" onclick="initMemeEditor(${img.id},this)" style="background-image: url('${img.url}')">
